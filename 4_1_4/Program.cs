e2dfea85515db941ed2171833795a0123127af7a
Andentu <severalmaz97@gmail.com>
2023-02-11 21:27:23 +0300

    Добавьте файлы проекта.

---
+++ 4_1_4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _4_1_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Task 4.1.4
+            string a = "text";
+            string b = "btext";
+            bool c = a != b;
+
+            // Task 4.1.5
+            int A = 43;
+            int B = 64;
+            double X = 32.3;
+            double Y = 155.4;
+            bool tr = (A < B) && (X > Y);
+            if (tr == true)
+            {
+                Console.WriteLine("tr");
+            }
+
+            // Task 4.1.12
+            if (A != B)
+            {
+                Console.WriteLine("Условие истинно");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+        }
+    }
+}
